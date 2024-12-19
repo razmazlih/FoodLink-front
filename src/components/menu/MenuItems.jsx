80bef9b87f46cf30845a2b31b7d06e1a8c4d66b9
Raz <retraz123@gmail.com>
2024-12-19 22:43:09 +0200

    order code

--- src/components/menu/MenuItems.jsx
+++ src/components/menu/MenuItems.jsx
@@ -1,18 +1,23 @@
 function MenuItems({ menu }) {
+    const hundleClickAddToCart = (menuItem) =>
+        console.log('Add to cart', menuItem.name, menuItem.id);
+
+    const showItems = (menuItem) => (
+        <li key={menuItem.id}>
+            <strong>{menuItem.name}</strong> - {menuItem.price} ₪{' '}
+            <button onClick={() => hundleClickAddToCart(menuItem)}>
+                Add to Cart
+            </button>
+            <p>{menuItem.description}</p>
+        </li>
+    );
     return (
         <div>
             <h2>Menu</h2>
             {menu.map((category) => (
                 <div key={category.id}>
                     <h3>{category.name}</h3>
-                    <ul>
-                        {category.items.map((menuItem) => (
-                            <li key={menuItem.id}>
-                                <strong>{menuItem.name}</strong> -{' '}
-                                {menuItem.price} ₪<p>{menuItem.description}</p>
-                            </li>
-                        ))}
-                    </ul>
+                    <ul>{category.items.map(showItems)}</ul>
                 </div>
             ))}
         </div>
