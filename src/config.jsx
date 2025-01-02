ef5498c602084b8187e54848cc9ba75bbe570f3f
Raz <retraz123@gmail.com>
2025-01-02 23:02:04 +0200

    add Userbase base url

--- src/config.jsx
+++ src/config.jsx
@@ -1,1 +1,2 @@
 export const API_DISHBOARD_URL = process.env.REACT_APP_API_DISHBOARD_URL || "http://localhost:8002/api";
+export const API_USERBASE_URL = process.env.REACT_APP_API_USERBASE_URL || "http://localhost:8001/api";
