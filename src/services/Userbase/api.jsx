4e7cfa51ffcca5d89d9a43afff1d7e590b9da27d
Raz <retraz123@gmail.com>
2025-01-02 23:29:47 +0200

    add userCredentials api function

---
+++ src/services/Userbase/api.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { API_USERBASE_URL } from "../../config";
+
+
+export const userCredentials = async (user) => {
+    try {
+        const { data } = await axios.post(`${API_USERBASE_URL}/login/`, user);
+        return data;
+    } catch (error) {
+        console.error("Error login user:", error);
+        throw error;
+    }
+};
