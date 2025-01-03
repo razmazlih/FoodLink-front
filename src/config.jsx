bd2d0370a11b062609ed3a3a3e27c105ba5c4f1e
Raz <retraz123@gmail.com>
2025-01-03 16:36:38 +0200

    add OrderLine base url

--- src/config.jsx
+++ src/config.jsx
@@ -1,2 +1,3 @@
+export const API_ORDERLINE_URL = process.env.REACT_APP_API_ORDERLINE_URL || "http://localhost:8003";
 export const API_DISHBOARD_URL = process.env.REACT_APP_API_DISHBOARD_URL || "http://localhost:8002/api";
 export const API_USERBASE_URL = process.env.REACT_APP_API_USERBASE_URL || "http://localhost:8001/api";
