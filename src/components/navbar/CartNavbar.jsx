3ff14eb6d0d92c6145fa44d117e72e082a413e1e
Raz <retraz123@gmail.com>
2025-01-03 19:36:48 +0200

    update showing cart

--- src/components/navbar/CartNavbar.jsx
+++ src/components/navbar/CartNavbar.jsx
@@ -2,11 +2,11 @@ import { useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
 
 function CartNavbar() {
-    const { cart } = useContext(CartContext);
+    let { cart } = useContext(CartContext);
     return (
         <div>
             <h2>My Cart</h2>
-            {cart ? cart : <h3>Your cart is empty</h3>}
+            {cart.length ? <h2>{cart}</h2> : <h3>cart is empty</h3>}
         </div>
     );
 }
