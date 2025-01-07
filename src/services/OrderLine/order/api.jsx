f84eb31d279b568c2386e059eef5ff27ec28cdde
Raz <retraz123@gmail.com>
2025-01-07 19:28:42 +0200

    delete unuse code

--- src/services/OrderLine/order/api.jsx
+++ src/services/OrderLine/order/api.jsx
@@ -2,9 +2,6 @@ import axios from 'axios';
 import { API_ORDERLINE_URL } from '../../../config';
 
 export const fetchOrder = async (orderId) => {
-    if (!orderId || orderId === null) {
-        return [];
-    }
     try {
         const { data } = await axios.get(
             `${API_ORDERLINE_URL}/orders/${orderId}`
