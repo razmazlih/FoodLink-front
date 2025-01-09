1379db121559b5bf932f73736d3270c97edfe6a6
Raz <retraz123@gmail.com>
2025-01-09 15:13:56 +0200

    fix switch carts

--- src/components/orders/all_orders/Orders.jsx
+++ src/components/orders/all_orders/Orders.jsx
@@ -1,15 +1,26 @@
 import { useContext, useEffect, useState } from 'react';
-import { deleteOrder, fetchAllOrders } from '../../../services/OrderLine/order/api';
+import {
+    deleteOrder,
+    fetchAllOrders,
+    fetchOrder,
+} from '../../../services/OrderLine/order/api';
 import { AuthContext } from '../../../context/AuthContext';
 import { getRestaurantsNamesAndIds } from '../../../services/DishBoard/restaurants/api';
 import { useNavigate } from 'react-router-dom';
 import './Orders.css'; // Import CSS file
+import { CartContext } from '../../../context/CartContext';
 
 function Orders() {
     const navigate = useNavigate();
     const { userId } = useContext(AuthContext);
+    const { updateCart } = useContext(CartContext);
     const [myOrders, setMyOrders] = useState([]);
-    const [restaurantMap, setRestaurantMap] = useState(JSON.parse(localStorage.getItem('restaurantMap')) || {});
+    const [restaurantMap, setRestaurantMap] = useState(
+        JSON.parse(localStorage.getItem('restaurantMap')) || {}
+    );
+    const [myReservations, setMyReservations] = useState(
+        JSON.parse(localStorage.getItem('myReservations')) || []
+    );
 
     useEffect(() => {
         fetchAllOrders(userId).then((orders) => setMyOrders(orders));
@@ -21,6 +32,16 @@ function Orders() {
             setRestaurantMap(map);
             localStorage.setItem('restaurantMap', JSON.stringify(map));
         });
+
+        if (userId) {
+            fetchAllOrders(userId).then((response) => {
+                setMyReservations(response);
+                localStorage.setItem(
+                    'myReservations',
+                    JSON.stringify(response)
+                );
+            });
+        }
     }, [userId]);
 
     const hundleDeleteOrder = (orderId) => {
@@ -28,23 +49,42 @@ function Orders() {
         setMyOrders(myOrders.filter((order) => order.id !== orderId));
     };
 
-    const hundleContinueOrdering = (orderId) => {
-        navigate(`/restaurants/${orderId}/`);
+    const hundleContinueOrdering = (restaurantId) => {
+        const reservation = myReservations.find(
+            (order) => order.restaurant_id === restaurantId
+        );
+
+        if (reservation) {
+            fetchOrder(reservation.id).then((order) => {
+                updateCart(order.items, order.id);
+                navigate(`/restaurants/${restaurantId}/`);
+            });
+        } else {
+            console.warn('No reservation found for this restaurant.');
+        }
     };
 
     const showingOrders = myOrders.map((order) => (
         <div key={order.id} className="order-item">
-            <h2>{restaurantMap[order.restaurant_id]} - {order.total_price}₪</h2>
-            <button className="continue-ordering" onClick={() => hundleContinueOrdering(order.restaurant_id)}>Continue Ordering</button>
-            <button className="delete-order" onClick={() => hundleDeleteOrder(order.id)}>Delete Order</button>
+            <h2>
+                {restaurantMap[order.restaurant_id]} - {order.total_price}₪
+            </h2>
+            <button
+                className="continue-ordering"
+                onClick={() => hundleContinueOrdering(order.restaurant_id)}
+            >
+                Continue Ordering
+            </button>
+            <button
+                className="delete-order"
+                onClick={() => hundleDeleteOrder(order.id)}
+            >
+                Delete Order
+            </button>
         </div>
     ));
 
-    return (
-        <div className="orders-container">
-            {showingOrders}
-        </div>
-    );
+    return <div className="orders-container">{showingOrders}</div>;
 }
 
 export default Orders;
