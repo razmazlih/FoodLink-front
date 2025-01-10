140e1c81118937f9fd55243cc6f0edd25e3a617a
Raz <retraz123@gmail.com>
2025-01-10 23:16:14 +0200

    add logo image in home page

--- src/components/home/FirstContent.jsx
+++ src/components/home/FirstContent.jsx
@@ -4,19 +4,13 @@ import './FirstContent.css';
 function TopContent() {
     return (
         <div className="first-content">
-            <h2
-            className="first-content-h2"
-            >Delicious deliveries</h2>
-            <p
-            className="first-content-p"
-            >Explore flavors at your doorstep</p>
+            <h2 className="first-content-h2">Delicious deliveries</h2>
+            <p className="first-content-p">Explore flavors at your doorstep</p>
             <Link to={'/restaurants'}>
-                <button
-                className="first-content-button">Order Now</button>
+                <button className="first-content-button">Order Now</button>
             </Link>
             <Link to={'/how-it-works'}>
-                <button
-                className="first-content-button">Our Services</button>
+                <button className="first-content-button">Our Services</button>
             </Link>
         </div>
     );
