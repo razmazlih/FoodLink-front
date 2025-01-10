487170e67b1a829aa538c269b2dce3018aff4c02
Raz <retraz123@gmail.com>
2025-01-11 00:04:03 +0200

    add links to docs of servers

--- src/components/how-it-works/HowItWorksMain.jsx
+++ src/components/how-it-works/HowItWorksMain.jsx
@@ -14,27 +14,39 @@ function HowItWorksMain() {
             <strong>User Management:</strong> Create an account or log in to
             access all features. Your account securely stores your preferences
             and order history for a personalized experience.
+            <br/>
+            Links: 
+            <a href="https://userbase-xqd2.onrender.com" target="_blank" rel="noopener noreferrer" className="service-link">Documentation</a> | 
+            <a href="https://github.com/razmazlih/UserBase" target="_blank" rel="noopener noreferrer" className="service-link">GitHub</a>
           </li>
           <li className="step-item">
             <strong>Restaurant Management:</strong> Browse through a curated
             list of restaurants. View detailed menus with prices and
             descriptions to find exactly what you’re craving.
+            <br/>
+            Links: 
+            <a href="https://dishboard.onrender.com" target="_blank" rel="noopener noreferrer" className="service-link">Documentation</a> | 
+            <a href="https://github.com/razmazlih/DishBoard" target="_blank" rel="noopener noreferrer" className="service-link">GitHub</a>
           </li>
           <li className="step-item">
             <strong>Order Management:</strong> Add items to your cart from one
             or multiple restaurants. Keep track of all your ongoing orders
             simultaneously. After confirming your order, track its status
             in real-time until completion.
+            <br/>
+            Links: 
+            <a href="https://orderline.onrender.com/docs" target="_blank" rel="noopener noreferrer" className="service-link">Documentation</a> | 
+            <a href="https://github.com/razmazlih/OrderLine" target="_blank" rel="noopener noreferrer" className="service-link">GitHub</a>
           </li>
           <li className="step-item">
             <strong>Real-Time Updates:</strong> Using WebSocket technology, we
             provide live updates on your delivery status, so you always know
-            when your food will arrive.
+            when your food will arrive. 
           </li>
           <li className="step-item">
             <strong>Simplified Payments:</strong> The platform includes a
             simulated payment system to finalize your orders securely and
-            efficiently.
+            efficiently. 
           </li>
           <li className="step-item">
             <strong>Built for Learning:</strong> This project was developed to
