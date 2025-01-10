d94d3debb5a7d6b43825830c828d5eaf83dc3f23
Raz <retraz123@gmail.com>
2025-01-10 11:40:03 +0200

    update checkout page

--- src/components/about/AboutMain.jsx
+++ src/components/about/AboutMain.jsx
@@ -1,9 +1,27 @@
 function AboutMain() {
-  return (
-    <div>
-      <h1>About</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>About</h1>
+            <p>
+                I developed an online food ordering website using Django and
+                FastAPI, integrating WebSocket in FastAPI for real-time updates
+                on delivery status. The website includes three server-side
+                services: user management, restaurant management, and order
+                management. The primary goal of the website was to experiment
+                with complex development involving multiple servers. Key
+                features of the site include user registration and login,
+                viewing restaurant menus and the list of restaurants, tracking
+                multiple orders simultaneously, and monitoring order status
+                after payment (simulated payment). I worked on the project alone
+                to learn and experiment with all aspects of development. I used
+                three PostgreSQL databases and didn’t integrate any external
+                tools. I designed the website independently. The biggest
+                challenge was aggregating data from multiple servers and
+                presenting it as coherent information on the client side. The
+                website is for personal use and learning, so it is not active.
+            </p>
+        </div>
+    );
 }
 
-export default AboutMain
+export default AboutMain;
