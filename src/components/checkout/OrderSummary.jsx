9074f6b9dac11fc2a164747b3e5edd898b1a9e00
Raz <retraz123@gmail.com>
2025-01-10 18:17:56 +0200

    reset my cart when checkout cart

--- src/components/checkout/OrderSummary.jsx
+++ src/components/checkout/OrderSummary.jsx
@@ -1,13 +1,22 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { updateCartStatus } from "../../services/OrderLine/status/api";
+import { useNavigate, useParams } from 'react-router-dom';
+import { updateCartStatus } from '../../services/OrderLine/status/api';
+import { CartContext } from '../../context/CartContext';
+import { useContext } from 'react';
 
 function OrderSummary({ totalPrice }) {
     const { orderId } = useParams();
+    const { updateCart } = useContext(CartContext);
     const navigate = useNavigate();
 
     const handleCheckout = () => {
         updateCartStatus(orderId)
-        navigate('/my-orders')
+            .then(() => {
+                updateCart([], '');
+                navigate('/my-orders');
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     };
 
     return (
