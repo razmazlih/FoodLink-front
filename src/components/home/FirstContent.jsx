6a5c1988fbcfb1af1cb2475ab9cc674cfc78207c
Raz <retraz123@gmail.com>
2025-01-11 20:56:40 +0200

    update text in firstContent component

--- src/components/home/FirstContent.jsx
+++ src/components/home/FirstContent.jsx
@@ -5,7 +5,7 @@ function TopContent() {
     return (
         <div className="first-content">
             <h2 className="first-content-h2">Delicious deliveries</h2>
-            <p className="first-content-p">Explore flavors at your doorstep</p>
+            <p className="first-content-p">Hot, fresh, and ready to go – get your meal today!</p>
             <div>
             <Link to={'/restaurants'}>
                 <button className="first-content-button">Order Now</button>
